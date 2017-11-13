d925d731da64938fab487a226bf0f7250ee3ebd1
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2017-11-13 22:57:51 +0530

    FOSS.java added

---
+++ Student-Diary/src/studentDiary/FOSS.java
@@ -0,0 +1,38 @@
+package studentDiary;
+
+import java.util.Scanner;
+
+public class FOSS implements Club {
+    Student s;
+    boolean member;
+    String team;
+    String url;
+    String field;
+    
+    Scanner sc = new Scanner(System.in);
+    
+    FOSS() {
+        member = false;
+    }
+    
+    public void addDetail() {
+        System.out.println("Enter team, project link and field");
+        member = true;
+        team = sc.next();
+        url = sc.next();
+        field = sc.next();
+    }
+    
+    public void addStudent(Student s) {
+        this.s = s;
+    }
+    
+    public String toString() {
+        return "Active member: " + member + "\nProject url: " + url + "\nField of Expertise: " + field + "\n";
+    }
+    
+    public void print() {
+        System.out.println(this.toString());
+    }
+    
+}
